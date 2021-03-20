c4b6ab204dfa9754ebbab86eecc2b6fbc448433f
rafaalms <rafael.profeng@gmail.com>
2021-03-20 18:22:13 -0300

    add UsersController

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -1,1 +1,25 @@
-export default class UsersController {}
+import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+import ListUsersService from '../services/ListUsersService';
+
+export default class UsersController {
+	public async index(request: Request, response: Response): Promise<Response> {
+		const listUsersServicde = new ListUsersService();
+
+		const users = await listUsersServicde.execute();
+
+		return response.json(users);
+	}
+
+	public async create(request: Request, response: Response): Promise<Response> {
+		const { name, email, password } = request.body;
+		const createUserService = new CreateUserService();
+
+		const newUser = await createUserService.execute({
+			name,
+			email,
+			password,
+		});
+		return response.json(newUser);
+	}
+}
--- src/modules/users/services/ListUsersService.ts
+++ src/modules/users/services/ListUsersService.ts
@@ -2,7 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 
-export default class ListProductService {
+export default class ListUsersService {
 	public async execute(): Promise<User[]> {
 		const usersRepository = getCustomRepository(UsersRepository);
 
